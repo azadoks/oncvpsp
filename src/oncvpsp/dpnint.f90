azadoks/oncvpsp

commit c971e1b4df75b00986d78ffc611a98f702db1a4a

Use constants_m's definition of dp universally

src/oncvpsp/dpnint.f90
@@ -23,13 +23,11 @@ subroutine dpnint(xx, yy, nn, tt, ss, mm)
     ! npoly sets order of polynomial
     ! xx must be ordered in ascending order
     ! output mm interpolated values ss on points tt
-
+    use constants_m, only: dp
     implicit none
 
-    integer, parameter :: dp = kind(1.0d0)
-
     !Input variables
-    real(dp) :: xx(*), yy(*), tt(*)
+    real(dp), dimension(:) :: xx, yy, tt
     integer :: nn, mm
 
     !Output variables

src/oncvpsp/check_data.f90
@@ -26,8 +26,9 @@ subroutine check_data(atsym, zz, fcfact, rcfact, epsh1, epsh2, depsh, rlmax, drl
     !*cnf variables are arrays of the basis atomic configuration variables
     !to be used for the test configurations
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     ! Input variables
     character*2 :: atsym

src/oncvpsp/const_basis.f90
@@ -35,8 +35,9 @@ subroutine const_basis(nbas, ncon, cons, orbasis, orbasis_der,&
     !          the pswf0 combination (which is not normalized)
     !ps0norm  charge contained in ps0 inside rc
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     !input variables
     integer :: ncon, nbas

src/oncvpsp/der2exc.f90
@@ -38,8 +38,9 @@ subroutine der2exc(rhotot, rhoc, rho, rr, d2exc, d2ref, d2mdiff, &
     ! ircut  maximum-radius for which all-electron and pseudo charges differ
     ! mmax  dimensiion of log grid
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     ! Input variables
     real(dp) :: rhotot(mmax), rhoc(mmax), rho(mmax, nv), rr(mmax)

src/oncvpsp/exc_libxc.f90
@@ -114,8 +114,9 @@ end subroutine exc_libxc
 subroutine derivs(mmax, rho, al, rr, dpr, dppr, dlap)
     ! NB: presumes incoming rho is properly scaled without 4pi or r**2 factors.
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     integer, intent(in) :: mmax
     real(dp), intent(in) :: al

src/oncvpsp/exc_libxc_stub.f90
@@ -22,9 +22,9 @@ subroutine exc_libxc(iexc, al, rho, vxc, exc, rr, mmax)
     !  dummy substitute for interface to libxc library to allow successful
     !  build without this library
     !
+    use constants_m, only: dp
     implicit none
 
-    integer, parameter :: dp = kind(1.0d0)
 
     real(dp), intent(in) :: al
     real(dp), intent(in) :: rho(mmax), rr(mmax)

src/oncvpsp/fpovlp.f90
@@ -20,8 +20,9 @@
 
 subroutine fpovlp(gg, hh, nn, ll, zz, ss, rr, srel)
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     !gg wave function one
     !hh wave function two

src/oncvpsp/gnu_script.f90
@@ -26,8 +26,9 @@ subroutine gnu_script(epa, evkb, lmax, lloc, mxprj, nproj)
     !mxprj maximum number of projectors
     !nproj number of projectors for each l
 
+    use constants_m, only: dp
     implicit none
-    integer, parameter :: dp = kind(1.0d0)
+
 
     !Input variables
     integer :: lmax, lloc, mxprj

commit 438b7cb540d1060028eef59d1d6dd7fe325dfed4

Fix critical memory error in dpnint introduced in c971e1b4df75b00986d78ffc611a98f702db1a4a

src/oncvpsp/dpnint.f90
@@ -27,7 +27,7 @@ subroutine dpnint(xx, yy, nn, tt, ss, mm)
    implicit none
 
    !Input variables
-   real(dp), dimension(:) :: xx, yy, tt
+   real(dp) :: xx(*), yy(*), tt(*)
    integer :: nn, mm
 
    !Output variables